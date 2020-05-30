e6a97c54922cd2383780fb93984ea23719f03880
Akshat Agrawal <akshat9300@gmail.com>
2020-05-29 20:40:47 -0700

    Added Live Data Observer in game fragment kotlin file and view model variable in game fragment xml file

--- app/src/main/java/com/example/andriod/splock/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/andriod/splock/screens/game/GameFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.andriod.splock.R
 import com.example.andriod.splock.databinding.FragmentGameBinding
 
@@ -25,10 +28,22 @@ class GameFragment : Fragment() {
         // Initialize Binding VariableInflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container,false)
 
+        // Initialize ViewModel
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
+        viewModel.eventGameFinish.observe(viewLifecycleOwner, Observer { result ->
+            if(result == 1) {
+                findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
+            }
+            else if(result == 0) {
+                findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameLostFragment())
+            }
+            else {
+                findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameTiedFragment())
+            }
+        })
 
-
-
+        binding.gameViewModel = viewModel
         return binding.root
     }
 
